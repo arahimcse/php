380e91af8255fe282c44bbf147f3bd95293c27d3
Abdur Rahim <arahimcse@gmail.com>
2023-09-22 22:04:32 +0600

    Type System

---
+++ Types/typesystem.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>This is Type System</title>
+</head>
+<body>
+    <a href="https://www.php.net/manual/en/language.types.type-system.php">Source Link</a>
+   <?php
+   /**
+    * build in type
+    - null
+    scaler type
+    -bool
+    -int 
+    -float
+    -string
+    array
+    object
+    resource
+    void
+    never
+    relative class types: self , parent, static
+
+    iteral types
+    -false
+    true
+
+    user defined type
+    -class
+    -interface
+    -enumeration
+
+    callable
+    */
+   
+   ?>
+</body>
+</html>
