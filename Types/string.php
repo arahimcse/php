75d0befa1e98cdc4f41110dfad8745c0faa36dfa
Abdur Rahim <arahimcse@gmail.com>
2023-09-22 22:38:08 +0600

    string

---
+++ Types/string.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href="https://www.php.net/manual/en/language.types.string.php">Source Link</a>
+    <?php
+    /**
+     * string four types
+     * single quotated
+     * double quotated
+     * heredoc
+     * nowdoc
+     */
+    
+     
+    ?>
+</body>
+</html>
