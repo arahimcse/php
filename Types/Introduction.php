0cfe69affbc6c5d4c6871dba0eb7cabd06b18b79
Abdur Rahim <arahimcse@gmail.com>
2023-09-22 20:47:06 +0600

    array_walk

--- Types/Introduction.php
+++ Types/Introduction.php
@@ -31,6 +31,24 @@
      $array_number = array_map($double, $number);
 
      echo implode(' ', $array_number);
+
+     //array_walk
+     class mc{
+        public function go(array $arr)
+        {
+            array_walk($arr, array($this, 'Walk'));
+        }
+
+        private function Walk($val)
+        {
+            echo $val . "<br/>";
+        }
+     }
+
+     $data = [1,2,3,4];
+     $obj = new mc();
+     
+     $obj->go($data);
     ?>
 </body>
 </html>
