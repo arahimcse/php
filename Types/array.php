7017c144333920cdf30afa2941c752024ad00ab2
Abdur Rahim <arahimcse@gmail.com>
2023-09-23 23:10:53 +0600

    array

---
+++ Types/array.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href="https://www.php.net/manual/en/language.types.array.php">Source Link</a>
+    <?php 
+    /**
+     * array structure ---- 
+     * array()
+     * []
+     */
+    $arr = array(1,2,3,4,5,6);
+    foreach($arr as $value)
+    {
+        echo $value."<br/>";
+    }
+    
+    ?>
+</body>
+</html>
