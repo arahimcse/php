fc58beaec81897ac48e0d7804561abc9e43ef129
Abdur Rahim <arahimcse@gmail.com>
2023-09-26 10:32:08 +0600

    Array dereferencing

--- Types/array.php
+++ Types/array.php
@@ -1,32 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <a href="https://www.php.net/manual/en/language.types.array.php">Source Link</a>
-    <?php 
+    <?php
     /**
      * array structure ---- 
      * array()
      * []
      */
-    $arr = array(1,2,3,4,5,6);
-    foreach($arr as $value)
-    {
-        echo $value."<br/>";
+    $arr = array(1, 2, 3, 4, 5, 6);
+    foreach ($arr as $value) {
+        echo $value . "<br/>";
     }
-    
+
     //Example #7 Accessing array elements
     $array = array(
         "foo" => "bar",
         42    => 24,
         "multi" => array(
-             "dimensional" => array(
-                 "array" => "foo"
-             )
+            "dimensional" => array(
+                "array" => "foo"
+            )
         )
     );
 
@@ -43,6 +44,54 @@
     echo $a;
     echo $b;
     echo $c;
+
+    // Task 1:
+    $student = array(
+        'name' => 'Alice',
+        'age' => 22,
+        'grade' => 'A'
+    );
+    echo "Age of the student: " . $student['age'];
+
+    // Task 2:
+    $student = array(
+        'name' => 'Alice',
+        'age' => 22,
+        'grade' => 'A'
+    );
+    if (array_key_exists('grade', $student)) {
+        echo "'grade' key exists in the array.\n";
+    } else {
+        echo "'grade' key does not exist in the array.\n";
+    }
+
+    // Task 3:
+    $numbers = array(100, 200, 50, 40, 50);
+    foreach ($numbers as $number) {
+        echo $number . "\n";
+    }
+
+    //Task 4:
+    $names = ['Talha', 'Afnan', 'Mashrufa', 'Zia', 'Iqbal', 'Habib', 'Airin', 'Moni'];
+    $filteredNames = array_filter($names, function ($name) {
+        return substr($name, 0, 1) === 'M';
+    });
+    foreach ($filteredNames as $name) {
+        echo $name . "\n";
+    }
+
+    // Task 5:
+    $originalString = 'Hello, World!';
+    $reversedString = strrev($originalString);
+    echo $reversedString;
+
+    function getArray() {
+        return array(1, 2, 3);
+    }
+    
+    $secondElement = getArray()[1];
+
     ?>
 </body>
+
 </html>
